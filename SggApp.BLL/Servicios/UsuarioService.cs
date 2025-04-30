cd090071f466b36fdf45c3757d5318cca40e041a
SamuelM209 <161536034+SamuelM209@users.noreply.github.com>
2025-04-30 05:13:33 -0500

    Update UsuarioService.cs

--- SggApp.BLL/Servicios/UsuarioService.cs
+++ SggApp.BLL/Servicios/UsuarioService.cs
@@ -1,1 +1,48 @@
+using SggApp.BLL.Interfaces;
+using SggApp.DAL.Entidades;
+using SggApp.DAL.Repositorios;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SggApp.BLL.Servicios
+{
+    public class UsuarioService : IUsuarioService
+    {
+        private readonly UsuarioRepository _usuarioRepository;
+
+        public UsuarioService(UsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+
+        public async Task<IEnumerable<Usuario>> ObtenerTodosAsync()
+        {
+            return await _usuarioRepository.GetAllAsync();
+        }
+
+        public async Task<Usuario> ObtenerPorIdAsync(int id)
+        {
+            return await _usuarioRepository.GetByIdAsync(id);
+        }
+
+        public async Task AgregarAsync(Usuario usuario)
+        {
+            await _usuarioRepository.AddAsync(usuario);
+        }
+
+        public async Task ActualizarAsync(Usuario usuario)
+        {
+            _usuarioRepository.Update(usuario);
+        }
+
+        public async Task EliminarAsync(int id)
+        {
+            var usuario = await _usuarioRepository.GetByIdAsync(id);
+            if (usuario != null)
+            {
+                _usuarioRepository.Delete(usuario);
+            }
+        }
+    }
+}
  
