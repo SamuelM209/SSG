df2c712289622cb86d87f2e0e7579e4b9b37ec5d
SamuelM209 <161536034+SamuelM209@users.noreply.github.com>
2025-04-30 05:12:16 -0500

    Update IGastoService.cs

--- SggApp.BLL/Interfaces/IGastoService.cs
+++ SggApp.BLL/Interfaces/IGastoService.cs
@@ -1,1 +1,17 @@
+using SggApp.DAL.Entidades;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SggApp.BLL.Interfaces
+{
+    public interface IGastoService
+    {
+        Task<IEnumerable<Gasto>> ObtenerTodosAsync();
+        Task<Gasto> ObtenerPorIdAsync(int id);
+        Task<IEnumerable<Gasto>> ObtenerPorUsuarioAsync(int usuarioId);
+        Task AgregarAsync(Gasto gasto);
+        Task ActualizarAsync(Gasto gasto);
+        Task EliminarAsync(int id);
+    }
+}
  
