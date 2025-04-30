c4e4a33a797ae6b7688b1069964851db7dc1e4f9
SamuelM209 <161536034+SamuelM209@users.noreply.github.com>
2025-04-30 05:12:31 -0500

    Update ICategoriaService.cs

--- SggApp.BLL/Interfaces/ICategoriaService.cs
+++ SggApp.BLL/Interfaces/ICategoriaService.cs
@@ -1,1 +1,16 @@
+using SggApp.DAL.Entidades;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SggApp.BLL.Interfaces
+{
+    public interface ICategoriaService
+    {
+        Task<IEnumerable<Categoria>> ObtenerTodosAsync();
+        Task<Categoria> ObtenerPorIdAsync(int id);
+        Task AgregarAsync(Categoria categoria);
+        Task ActualizarAsync(Categoria categoria);
+        Task EliminarAsync(int id);
+    }
+}
  
