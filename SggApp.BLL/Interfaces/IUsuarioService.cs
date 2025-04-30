093d4e89f79811a620c0152c332ee60c09071503
SamuelM209 <161536034+SamuelM209@users.noreply.github.com>
2025-04-30 05:11:57 -0500

    Update IUsuarioService.cs

--- SggApp.BLL/Interfaces/IUsuarioService.cs
+++ SggApp.BLL/Interfaces/IUsuarioService.cs
@@ -1,1 +1,16 @@
+using SggApp.DAL.Entidades;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SggApp.BLL.Interfaces
+{
+    public interface IUsuarioService
+    {
+        Task<IEnumerable<Usuario>> ObtenerTodosAsync();
+        Task<Usuario> ObtenerPorIdAsync(int id);
+        Task AgregarAsync(Usuario usuario);
+        Task ActualizarAsync(Usuario usuario);
+        Task EliminarAsync(int id);
+    }
+}
  
