31b73d3aa76a0a6dc84c545af9da2645c3010546
SamuelM209 <161536034+SamuelM209@users.noreply.github.com>
2025-04-30 05:12:59 -0500

    Update IPresupuestoService.cs

--- SggApp.BLL/Interfaces/IPresupuestoService.cs
+++ SggApp.BLL/Interfaces/IPresupuestoService.cs
@@ -1,1 +1,16 @@
+using SggApp.DAL.Entidades;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SggApp.BLL.Interfaces
+{
+    public interface IPresupuestoService
+    {
+        Task<IEnumerable<Presupuesto>> ObtenerTodosAsync();
+        Task<Presupuesto> ObtenerPorIdAsync(int id);
+        Task AgregarAsync(Presupuesto presupuesto);
+        Task ActualizarAsync(Presupuesto presupuesto);
+        Task EliminarAsync(int id);
+    }
+}
  
