fc3566b5dcddd9ffac06d576e4ebe3ed0ef51ea1
SamuelM209 <161536034+SamuelM209@users.noreply.github.com>
2025-04-30 05:12:47 -0500

    Update IMonedaService.cs

--- SggApp.BLL/Interfaces/IMonedaService.cs
+++ SggApp.BLL/Interfaces/IMonedaService.cs
@@ -1,1 +1,16 @@
+using SggApp.DAL.Entidades;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SggApp.BLL.Interfaces
+{
+    public interface IMonedaService
+    {
+        Task<IEnumerable<Moneda>> ObtenerTodosAsync();
+        Task<Moneda> ObtenerPorIdAsync(int id);
+        Task AgregarAsync(Moneda moneda);
+        Task ActualizarAsync(Moneda moneda);
+        Task EliminarAsync(int id);
+    }
+}
  
